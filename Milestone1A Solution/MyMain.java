c75ca5a5a58ab497f587bca6d08ecd6daedb9915
Umang-19 <umangaggarwal537@gmail.com>
2022-03-08 12:20:10 +0530

    Milestone 1A

--- Milestone1A Solution/MyMain.java
+++ Milestone1A Solution/MyMain.java
@@ -1,5 +1,6 @@
 import org.yaml.snakeyaml.Yaml;
 import java.io.*;
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
@@ -8,11 +9,12 @@ import java.util.logging.SimpleFormatter;
 public class MyMain {
     private static Logger logger = Logger.getLogger("MyLog");
     private static FileHandler fh;
+//    private static LocalDateTime myObj = LocalDateTime.now();
 
     // Time Function
     public static void TimeFunction(String FunctionInput, long time) throws InterruptedException {
         // log entry would go here
-        logger.info(FunctionInput);
+        logger.info(LocalDateTime.now() + ";" + FunctionInput);
         Thread.sleep(time * 1000);
     }
 
@@ -36,54 +38,54 @@ public class MyMain {
 
         Map.Entry<?, ?> entry = data.entrySet().iterator().next();
         String root = (String) entry.getKey();
-        logger.info(root + " " + " Entry");
+        logger.info(LocalDateTime.now() + ";" + root + " " + " Entry");
 
         Map<?, ?> activitiesMap = (Map<?, ?>) data.get(root);
-//        System.out.println(activitiesMap.get("Activities"));
-
         Map<?, ?> allTaskMap = (Map<?, ?>) activitiesMap.get("Activities");
 
         // Iterating over all activities
         for (Object name : allTaskMap.keySet()) {
             String taskname = (String) name;
-            logger.info(taskname + " " + " Entry");
+            logger.info(LocalDateTime.now() + ";" + root + "." + taskname + " " + " Entry");
 
             if (taskname.startsWith("Task")) {
-                Map<?, ?> alltasks = (Map<?, ?>) allTaskMap.get(taskname);
-                Map<?, ?> mytask = (Map<?, ?>) alltasks.get("Inputs");
-
-                String FunctionInput = (String) mytask.get("FunctionInput");
-                String exectime = (String) mytask.get("ExecutionTime");
-                long time = Long.parseLong(exectime);
-//                System.out.println(FunctionInput + " " + time);
-
-                String str = root + "." + taskname + "." + "Executing " + FunctionInput + "(" + exectime + ")";
-                TimeFunction(str, time);
-
+                processTask(root, allTaskMap, taskname);
             } else {
-
-                Map<?, ?> activitiesMap2 = (Map<?, ?>) allTaskMap.get(taskname);
-                Map<?, ?> allTaskMap2 = (Map<?, ?>) activitiesMap2.get("Activities");
-
-                for (Object name2 : allTaskMap2.keySet()) {
-                    String taskname2 = (String) name2;
-//                    System.out.println("Task 2 = " + taskname2);
-                    logger.info(taskname2 + " " + " Entry");
-                    Map<?, ?> alltasks2 = (Map<?, ?>) allTaskMap2.get(taskname2);
-                    Map<?, ?> mytask2 = (Map<?, ?>) alltasks2.get("Inputs");
-
-                    String FunctionInput = (String) mytask2.get("FunctionInput");
-                    String exectime = (String) mytask2.get("ExecutionTime");
-                    long time = Long.parseLong(exectime);
-                    System.out.println(FunctionInput + " " + time);
-
-                    String str = root + "." + taskname + "." + taskname2 + "." + "Executing " + FunctionInput + "(" + exectime + ")";
-                    TimeFunction(str, time);
-                    logger.info(taskname2 + " " + " Exit");
-                }
-                logger.info(taskname + " " + " Exit");
+                processFlow(allTaskMap, taskname);
             }
         }
+
+        logger.info(LocalDateTime.now() + ";" + root + " " + " Exit");
+    }
+
+    // Process Flow
+    private static void processFlow(Map<?, ?> allTaskMap, String taskname) throws InterruptedException {
+        Map<?, ?> activitiesMap2 = (Map<?, ?>) allTaskMap.get(taskname);
+        Map<?, ?> allTaskMap2 = (Map<?, ?>) activitiesMap2.get("Activities");
+        for (Object name2 : allTaskMap2.keySet()) {
+            String taskname2 = (String) name2;
+            logger.info(LocalDateTime.now() + ";" + taskname2 + " " + " Entry");
+            if (taskname2.startsWith("Task")) {
+                processTask(taskname, allTaskMap2, taskname2);
+            } else {
+                processFlow(allTaskMap2, taskname2);
+            }
+        }
+        logger.info(LocalDateTime.now() + ";" + taskname + " " + " Exit");
+    }
+
+    // Process Task
+    private static void processTask(String root, Map<?, ?> allTaskMap, String taskname) throws InterruptedException {
+        Map<?, ?> alltasks = (Map<?, ?>) allTaskMap.get(taskname);
+        Map<?, ?> mytask = (Map<?, ?>) alltasks.get("Inputs");
+
+        String FunctionInput = (String) mytask.get("FunctionInput");
+        String exectime = (String) mytask.get("ExecutionTime");
+        long time = Long.parseLong(exectime);
+
+        String str = root + "." + taskname + " Executing TimeFunction"  + "(" + exectime + ")";
+        TimeFunction(str, time);
+        logger.info(LocalDateTime.now() + ";" + taskname + " " + " Exit");
     }
 }
 
